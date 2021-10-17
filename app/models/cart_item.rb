06d571281de3a0270456da9bb9ac1e074934266c
ngkfndkngd <huku.0033@gmail.com>
2021-10-17 14:17:13 +0000

    ルート、アソシエーション追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,4 @@
 class CartItem < ApplicationRecord
+  belongs_to :member
+  belongs_to :items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,31 @@ Rails.application.routes.draw do
     registrations: "member/registrations",
     sessions: "member/sessions"
   }
+  
+  namespace :master do
+    resources :items
+    resources :genres
+    resources :orders
+    patch "order_details/:id" => "order_details#update"
+    resources :members
+  end
+
+  namespace :public do
+    root "homes#top"
+    get "about" => "homes#about"
+    resources :items
+    resources :cart_items
+    delete "cart_items" => "cart_items#destroy_all"
+    resources :orders
+    get "orders/check" => "orders#check"
+    get "orders/thanks" => "orders#thanks"
+    resource :members
+    get "members/out" => "members#out"
+    patch "members/checkout" => "members#checkout"
+    resources :deliveries
+  end	
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
