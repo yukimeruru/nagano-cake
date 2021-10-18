d95a3b56528aa3e62e61132e5bb1f7972457744a
yukimeruru <yuki725421@gmail.com>
2021-10-18 11:20:44 +0000

    追加

--- app/controllers/master/genres_controller.rb
+++ app/controllers/master/genres_controller.rb
@@ -1,15 +1,24 @@
 class Master::GenresController < ApplicationController
   
   def create
+    genre =Genre.new(genre_params)
+    genre.save
+    redirect_to master_genres_path
   end
   
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
   
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to master_genres_path
   end  
   
   private
