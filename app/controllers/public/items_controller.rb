ef156c34e53c63c37c06c9fb9649e603f899888a
supernemutai <tokikake66@gmail.com>
2021-10-19 01:14:41 +0000

    配送先一覧/登録、商品一覧追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,14 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
-  
+
   def show
   end
-  
+
   private
-  
+
     def item_params
       params.require(:item).permit(:name, :description, :image, :price, :sales_status)
     end
