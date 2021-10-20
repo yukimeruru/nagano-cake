636217ba7d702369cc69b237ea536cc51798e3a0
ngkfndkngd <huku.0033@gmail.com>
2021-10-20 07:35:55 +0000

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
-  devise_for :members, skip: [:passwords,], controllers:{
-    registrations: "member/registrations",
-    sessions: "member/sessions"
+  devise_for :members, skip: :all,controllers: {
+    
   }
+
+    devise_scope :member do
+      get "members/sign_up" => "member/registrations#new",as: :new_member_registration
+      post "members/sign_up" => "member/registrations#create"
+      get "members/sign_in" => "member/sessions#new",as: :new_member_session
+      post "members/sign_in" => "member/sessions#create"
+      delete "members/sign_out" => "member/sessions#destroy",as: :destroy_member_session
+    end
+
 
   namespace :master do
     resources :items
@@ -15,7 +24,6 @@ Rails.application.routes.draw do
     patch "order_items/:id" => "order_items#update"
     resources :members
   end
-
 
   scope module: :public do
     root "homes#top"
@@ -31,5 +39,7 @@ Rails.application.routes.draw do
     patch "members/checkout" => "members#checkout"
     resources :deliveries
   end
+
+
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
