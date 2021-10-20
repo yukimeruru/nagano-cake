1717653ff8bb269c649ade03f8aa58c197b90c52
hanamaru7830 <xwsmilexw@gmail.com>
2021-10-20 00:56:01 +0000

    会員詳細と編集ページ追加

--- app/controllers/master/members_controller.rb
+++ app/controllers/master/members_controller.rb
@@ -5,14 +5,18 @@ class Master::MembersController < ApplicationController
   end
 
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def update
+    member = Member.find(params[:id])
+    member.update(member_params)
+    redirect_to master_members_path
   end
-
 
   private
 
