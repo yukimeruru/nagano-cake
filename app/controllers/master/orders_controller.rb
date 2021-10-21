bd9a251c7bb7ce38db942a6bb3841d9690a94db2
supernemutai <tokikake66@gmail.com>
2021-10-21 06:41:31 +0000

    Start 管理者注文履歴詳細

--- app/controllers/master/orders_controller.rb
+++ app/controllers/master/orders_controller.rb
@@ -1,13 +1,14 @@
 class Master::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
   end
-  
+
   def update
   end
-  
+
   private
-  
+
     def order_params
       params.require(:order).permit(:shipping, :final_price, :payment_method, :name, :post_code, :address, :order_status)
     end
