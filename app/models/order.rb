ba78180232148cf3219990a6680662a5f159a730
ngkfndkngd <huku.0033@gmail.com>
2021-10-22 15:23:35 +0000

    管理者/注文履歴一覧

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,4 @@ class Order < ApplicationRecord
   enum order_status: {wait: 0, verification: 1, production: 2, delivery_wait: 3, completion: 4 }
   # {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
 
-
-
 end
