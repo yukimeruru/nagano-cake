521985f592fa17d2cbeaeb3f5fa869d43556b308
yuki-meruru <yuki725421@gmail.com>
2021-10-22 15:52:33 +0000

    レイアウト完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,6 +9,17 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.member_id = current_member.id
     @order.save
+
+    current_member.cart_items.each do |cart_item|
+      @order_item = OrderItem.new
+      @order_item.item_id = cart_item.item_id
+      @order_item.count = cart_item.count
+      @order_item.price = (cart_item.item.price*1.1*cart_item.count).floor
+      @order_item.order_id =  @order.id
+      @order_item.save
+    end
+
+    current_member.cart_items.destroy_all
     redirect_to orders_thanks_path
   end
 
@@ -48,6 +59,10 @@ class Public::OrdersController < ApplicationController
 
     def order_params
       params.require(:order).permit(:shipping, :final_price, :name, :post_code, :address, :order_status)
+    end
+
+    def order_item_params
+      params.require(:order_item).permit(:item_id, :order_id, :price, :count, :make_status)
     end
 
 end
