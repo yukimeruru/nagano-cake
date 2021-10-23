0a4b41201478f0d7038df67b936f1fb2a3f4f106
tachi7830 <xwsmilexw@gmail.com>
2021-10-23 04:54:01 +0000

    管理者側のログイン後遷移先追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :member
-
   has_many :order_items, dependent: :destroy
   
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
   enum order_status: { not_pay: 0, check_pay: 1, make: 2, ready: 3, shipped: 4 }
-
 end
