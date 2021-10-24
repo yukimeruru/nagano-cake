48b1ea60b49d408519011622ca6c9eb4cd1a1880
yuki-meruru <yuki725421@gmail.com>
2021-10-24 08:45:51 +0000

    大体完成２

--- app/controllers/member/registrations_controller.rb
+++ app/controllers/member/registrations_controller.rb
@@ -54,7 +54,14 @@ class Member::RegistrationsController < Devise::RegistrationsController
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
-
+  def after_sign_up_path_for(resource)
+      case resource
+      when Admin
+        master_orders_path
+      when Member
+        members_path
+      end
+  end
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
