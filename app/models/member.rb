3cef4a3b8e0d589d170bfb084bd93ad4d4eae0df
ngkfndkngd <huku.0033@gmail.com>
2021-10-25 01:05:32 +0000

    退会後の遷移先を変更

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,6 +8,7 @@ class Member < ApplicationRecord
   has_many :deliveries, dependent: :destroy
   has_many :orders, dependent: :destroy
   
+  
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
